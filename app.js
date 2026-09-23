6eb0562701f0de2162dce26c1a802d11e4d7ec64
SayedMOO <anaelsaid@gmail.com>
2026-09-23 17:29:49 +0300

    Use CDN for Swagger UI on Vercel

--- app.js
+++ app.js
@@ -60,12 +60,33 @@ const swaggerOptions = {
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use(
-    '/api-docs',
-    swaggerUi.serveFiles(swaggerSpec),
-    swaggerUi.setup(swaggerSpec)
-);
+app.get('/api-docs', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>E-Commerce API Documentation</title>
+            <link rel="stylesheet"
+                href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+        </head>
 
+        <body>
+            <div id="swagger-ui"></div>
+
+            <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+
+            <script>
+                window.onload = () => {
+                    window.ui = SwaggerUIBundle({
+                        spec: ${JSON.stringify(swaggerSpec)},
+                        dom_id: '#swagger-ui'
+                    });
+                };
+            </script>
+        </body>
+        </html>
+    `);
+});
 
 app.use(logger('dev'));
 
